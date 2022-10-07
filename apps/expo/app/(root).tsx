4fbd9f76296c3eef5c3128fe81b4c16b79933045
Fernando Rojo <frojo@sas.upenn.edu>
2022-10-07 12:58:11 -0400

    1️⃣ Chapter 1.3: Style our navigation stack

--- apps/expo/app/(root).tsx
+++ apps/expo/app/(root).tsx
@@ -1,10 +1,45 @@
 import { NativeStack as Stack } from 'expo-router'
 import { Provider } from 'app/provider'
 
+import { useDripsyTheme } from 'dripsy'
+import { StatusBar } from 'expo-status-bar'
+
+function MyStack() {
+  const { colors } = useDripsyTheme().theme
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.$background2,
+        },
+        headerTitleStyle: {
+          color: colors.$text,
+        },
+        headerTintColor: '#D864D8',
+        headerShadowVisible: false,
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: 'Home',
+        }}
+      />
+      <Stack.Screen
+        name="users"
+        options={{
+          title: 'User',
+        }}
+      />
+    </Stack>
+  )
+}
+
 export default function Root() {
   return (
     <Provider>
-      <Stack />
+      <MyStack />
+      <StatusBar style="light" />
     </Provider>
   )
 }
