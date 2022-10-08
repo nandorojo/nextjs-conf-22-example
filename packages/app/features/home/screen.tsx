9bbe060b116ba3a39d0efa7eac95aac6ee94abb6
Fernando Rojo <frojo@sas.upenn.edu>
2022-10-08 18:01:16 -0400

    fix dripsy

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,28 +1,30 @@
+import { Provider } from 'app/provider'
 import { Text, View } from 'dripsy'
 import { Link } from 'solito/link'
 
 export function HomeScreen() {
   return (
-    <View
-      sx={{
-        flex: 1,
-        bg: '$background',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Link href="/users/fernando">
-        <Text
-          sx={{
-            py: '$2',
-            px: '$3',
-            borderRadius: '$rounded',
-            bg: '$purple3',
-          }}
-        >
-          Hello Solito
-        </Text>
-      </Link>
-    </View>
+    <Provider>
+      <View
+        sx={{
+          flex: 1,
+          bg: '$background',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Link href="/users/fernando">
+          <Text
+            sx={{
+              py: '$2',
+              px: '$3',
+              bg: '$purple3',
+            }}
+          >
+            Hello Solito
+          </Text>
+        </Link>
+      </View>
+    </Provider>
   )
 }
--- packages/app/features/user/detail-screen.tsx
+++ packages/app/features/user/detail-screen.tsx
@@ -1,3 +1,4 @@
+import { Provider } from 'app/provider'
 import { View, Text } from 'dripsy'
 import { createParam } from 'solito'
 import { Link } from 'solito/link'
@@ -8,22 +9,21 @@ export function UserDetailScreen() {
   const [id] = useParam('id')
 
   return (
-    <View
-      sx={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        bg: '$background',
-      }}
-    >
-      <Link href="/">
-        <Text sx={{ textAlign: 'center' }}>
-          {`User ID:`}{' '}
-          <Text sx={{ bg: '$purple3', px: '$1', borderRadius: '$1' }}>
-            {id}
+    <Provider>
+      <View
+        sx={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          bg: '$background',
+        }}
+      >
+        <Link href="/">
+          <Text sx={{ textAlign: 'center' }}>
+            {`User ID:`} <Text sx={{ bg: '$purple3', px: '$1' }}>{id}</Text>
           </Text>
-        </Text>
-      </Link>
-    </View>
+        </Link>
+      </View>
+    </Provider>
   )
 }
